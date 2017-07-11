62600e671be57a1ecf14d8c251390cc96fc64c8b
dario <dario.meniss@slobodni-programi.hr>
2017-07-11 18:15:29 +0200

    hr_holidays_py3o (lenac demo)

---
+++ hr_holidays_py3o/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================
+Hr Leaves Py3o Report
+=====================
+
+Generate Leaves report with Py3o reporting engine
---
+++ hr_holidays_py3o/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Decodio Applications.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Hr Leaves Py3o Report',
+    'version': '10.0.1.0.0',
+    'category': 'Human Resources',
+    'license': 'AGPL-3',
+    'summary': 'Generate Leaves report with Py3o reporting engine',
+    'author': 'Decodio Applications',
+    'website': 'http://www.slobodni-programi.hr',
+    'depends': ['report_py3o'],
+    'data': [
+        'report/report_py3o.xml',
+    ],
+    'installable': True,
+}
