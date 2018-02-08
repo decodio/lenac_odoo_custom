926e67bfd98825a3e27ead5f02a23ad451aeab9f
Goran Kliska <goran.kliska@slobodni-programi.hr>
2018-02-08 16:37:59 +0100

    Fix depends:  multi line, remove redundant

--- vl_subcontractor/__manifest__.py
+++ vl_subcontractor/__manifest__.py
@@ -10,7 +10,11 @@
     'complexity': "normal",
     'author': 'Viktor Lenac d.d. (Vedran Terihaj)',
     'website': 'http://lenac.hr',
-    'depends': ['sp_viktor_lenac_dms_qms', 'website_partner', 'website_mail', 'website_form', 'survey', 'website_form_builder',],  # List dependent modules, sp_viktor_lenac_dms_qms is a safe bet
+    'depends': ['sp_viktor_lenac_dms_qms',
+                'website_partner',
+                'website_mail',
+                'survey',
+                'website_form_builder',],  # List dependent modules, sp_viktor_lenac_dms_qms is a safe bet
     'data': [
              'security/ir.model.access.csv',
              'security/website_vl_subcontractor_security.xml',
