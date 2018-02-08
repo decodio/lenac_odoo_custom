a4854d0977d745f5e727b81bed51b550a0bd364b
vedranik <vedran.ikica@lenac.hr>
2018-02-08 13:05:52 +0100

    Fix code

--- vl_subcontractor/models/vl_subcontractor.py
+++ vl_subcontractor/models/vl_subcontractor.py
@@ -237,15 +237,15 @@ class VlSubcontractor(models.Model):
 
     description = fields.Text("Description")
 
-    #@api.multi
-    #def action_start_survey(self):
-    #    self.ensure_one()
-    #    # create a response and link it to this applicant
-    #    if not self.response_id:
-    #        response = self.env['survey.user_input'].create(
-    #            {'survey_id': self.survey_id.id, 'partner_id': self.partner_id.id})
-    #        self.response_id = response.id
-    #    else:
-    #        response = self.response_id
-    #    # grab the token of the response and start surveying
-    #    return self.survey_id.with_context(survey_token=response.token).action_start_survey()
+    @api.multi
+    def action_start_survey(self):
+        self.ensure_one()
+        # create a response and link it to this applicant
+        if not self.response_id:
+            response = self.env['survey.user_input'].create(
+                {'survey_id': self.survey_id.id, 'partner_id': self.partner_id.id})
+            self.response_id = response.id
+        else:
+            response = self.response_id
+        # grab the token of the response and start surveying
+        return self.survey_id.with_context(survey_token=response.token).action_start_survey()
