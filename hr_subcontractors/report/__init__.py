c13eb220432dd05d6db3eda673dc36d3fab5f0f5
vedranik <vedran.ikica@lenac.hr>
2018-05-08 14:04:09 +0200

    Fix code

--- hr_subcontractors/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Odoo, Open Source Management Solution
-# Copyright (C) 2016 Decodio
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import vl_hr_evaluation_report
