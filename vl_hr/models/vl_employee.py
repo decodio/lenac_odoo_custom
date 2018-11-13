72f713e0bc4d1293c4df7b1793f937e50e91abff
vedranik <vedran.ikica@lenac.hr>
2018-11-13 09:50:57 +0100

    added dep_code to department

--- vl_hr/models/vl_employee.py
+++ vl_hr/models/vl_employee.py
@@ -7,3 +7,8 @@ class VLHREmployee(models.Model):
     _inherit = 'hr.employee'
 
     employee_number = fields.Char(string='Employee ID number')
+
+class VLHRDepartment(models.Model):
+    _inherit = 'hr.department'
+
+    dep_code = fields.Cahar(string="Department Code")
