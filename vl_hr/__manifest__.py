fb3e0c9cd7c12133645b3b57a3064cecc1fcdad0
vedranik <vedran.ikica@lenac.hr>
2018-11-13 11:22:05 +0100

    edited manifest

--- vl_hr/__manifest__.py
+++ vl_hr/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Employee job descripction and code (Vedran Terihaj)',
+    'name': 'VL Employee job description and code (Vedran Terihaj)',
     'version': '10.0.1.0.0',
     'category': 'Custom Lenac',
+    'complexity': 'normal',
     'author': "Viktor Lenac d.d. (Vedran Terihaj)",
-    'website': 'https://www.lenac.h',
+    'website': 'https://lenac.hr',
     'license': '',
     'depends': ['hr', ],
     'data': [
@@ -15,4 +16,7 @@
     'test': [],
     'installable': True,
     'auto_install': False,
+    'description': """
+        Extends the module for Shipyard Viktor Lenac""",
+    'summary': 'ICT Inventory tracking',
 }
