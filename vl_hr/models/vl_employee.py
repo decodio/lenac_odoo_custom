a2ea2e183af63d38f2ff358d93b98ace5eb17bd9
vedranik <vedran.ikica@lenac.hr>
2018-11-13 10:28:44 +0100

    fix code

--- vl_hr/models/vl_employee.py
+++ vl_hr/models/vl_employee.py
@@ -6,10 +6,10 @@ from odoo import models, fields
 class VLHREmployee(models.Model):
     _inherit = 'hr.employee'
 
-    employee_number = fields.Char(string='Employee ID number')
+    employee_number = fields.Char(string='Employee ID number', required=True)
 
 
 class VLHRDepartment(models.Model):
     _inherit = 'hr.department'
 
-    dep_code = fields.Char(string="Department Code")
+    dep_code = fields.Char(string="Department Code", required=True)
