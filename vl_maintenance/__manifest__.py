8de43e2c71ab5ba6f5f6944ff2defb17d0d26edf
vedranik <vedran.ikica@lenac.hr>
2018-11-14 08:48:34 +0100

    Added security

--- vl_maintenance/__manifest__.py
+++ vl_maintenance/__manifest__.py
@@ -14,6 +14,7 @@
                 ],
     'data': [
             'security/ir.model.access.csv',
+            'security/vl_maintenance.xml',
             'report/report.xml',
             'report/personal_equipment.xml',
             'views/vl_maintenance_views.xml',
