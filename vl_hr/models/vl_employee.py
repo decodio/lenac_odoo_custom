0ac8f80f795a935c1f092227824821c02932b099
vedranik <vedran.ikica@lenac.hr>
2018-11-14 11:25:13 +0100

    added styles for subcontractor module

--- vl_hr/models/vl_employee.py
+++ vl_hr/models/vl_employee.py
@@ -7,9 +7,11 @@ class VLHREmployee(models.Model):
     _inherit = 'hr.employee'
 
     employee_number = fields.Char(string='Employee ID number', required=True)
+    empass_equipement_ids = fields.One2many('maintenance.equipment', 'employee_id', string='Assigned equipment')
 
 
 class VLHRDepartment(models.Model):
     _inherit = 'hr.department'
 
     dep_code = fields.Char(string="Department Code", required=True)
+    depass_equipement_ids = fields.One2many('maintenance.equipment', 'department_id', string='Assigned equipment')
