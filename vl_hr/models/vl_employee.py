181781776c89f63997e2541b7af6b6df482cce3a
vedranik <vedran.ikica@lenac.hr>
2018-11-16 16:39:47 +0100

    code change

--- vl_hr/models/vl_employee.py
+++ vl_hr/models/vl_employee.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 from odoo import models, fields
 
 
@@ -9,11 +10,14 @@ class HrEmployee(models.Model):
     employee_number = fields.Char(string='Employee ID number', required=True)
 
     # empass_equipement_ids = fields.Many2one('maintenance.equipment', 'employee_id', string='Assigned equipment')
-    assigned_equipement_ids = fields.One2many('maintenance.equipment', 'employee_id', string='Assigned equipment')
+    employee_assigned_equipment_ids = fields.One2many('maintenance.equipment', 'employee_id',
+                                                      string='Assigned equipment')
 
 
 class HrDepartment(models.Model):
     _inherit = 'hr.department'
 
-    dep_code = fields.Char(string="Department Code", required=True)
-    depass_equipement_ids = fields.Many2one('maintenance.equipment', 'department_id', string='Assigned equipment')
+    department_code = fields.Char(string="Department Code", required=True)
+
+    department_assigned_equipment_ids = fields.One2many('maintenance.equipment', 'department_id',
+                                                        string='Assigned equipment')
