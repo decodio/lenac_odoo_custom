cb90109f19a9cc9982d8128c6413a0781ddb5dc1
VedranTe <vedran.terihaj@lenac.hr>
2019-01-24 14:01:09 +0100

    Fix

--- vl_hr/__manifest__.py
+++ vl_hr/__manifest__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+# Odoo, Open Source Management Solution
+# Copyright (C) 2018 Vedran Terihaj
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'VL Employee job description and code (Vedran Terihaj)',
     'version': '10.0.1.0.0',
     'category': 'Custom Lenac',
     'complexity': 'normal',
-    'author': "Viktor Lenac d.d. (Vedran Terihaj)",
+    'author': 'Viktor Lenac d.d. (Vedran Terihaj)',
     'website': 'https://lenac.hr',
     'license': '',
     'depends': ['hr', 'website_portal',],
--- vl_hr/models/vl_employee.py
+++ vl_hr/models/vl_employee.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Odoo, Open Source Management Solution
+# Copyright (C) 2018 Vedran Terihaj
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 from odoo import models, fields
