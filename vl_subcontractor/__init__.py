dbf283e38c68ea0c01345a80e4d6b7d065c6efd7
VedranTe <vedran.terihaj@lenac.hr>
2019-01-24 14:00:33 +0100

    Form and label name chane

--- vl_subcontractor/__init__.py
+++ vl_subcontractor/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Odoo, Open Source Management Solution
-# Copyright (C) 2017 Viktor Lenac
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).#
+# Copyright (C) 2018 Vedran Terihaj
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
 from . import controllers
--- vl_subcontractor/controllers/__init__.py
+++ vl_subcontractor/controllers/__init__.py
@@ -1,1 +1,6 @@
+# -*- coding: utf-8 -*-
+# Odoo, Open Source Management Solution
+# Copyright (C) 2018 Vedran Terihaj
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import main
