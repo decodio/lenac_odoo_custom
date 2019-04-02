7bfe07497bb7dc27aae29ba777f548a3b88fb027
VedranTe <vedran.terihaj@lenac.hr>
2019-04-02 12:28:27 +0200

    Menu for document exchange with Palumbo group.

---
+++ vl_palumbo_exchange/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "VL Palumbo exchange folders",
+    'version': '10.0.0.1.0',
+    'maintainer': 'Viktor Lenac',
+    'category': 'Tools',
+    'website': '',
+    'sequence': 1,
+    'description': """
+Uninstall this module
+    """,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'depends': ['cmis_field',
+                'cmis_alf'],
+    'data': ['views/vl_palumbo_exchange.xml',
+             ],
+
+}
