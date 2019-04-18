a88b1abe2558b4e5346a73e12e562c0fe1cdad99
VedranTe <vedran.terihaj@lenac.hr>
2019-04-18 15:16:38 +0200

    Testing

--- vl_energy_mgmt/models/energy_counter.py
+++ vl_energy_mgmt/models/energy_counter.py
@@ -3,22 +3,36 @@
 # Copyright (C) 2018 Vedran Terihaj
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
 
 
 class EnergyCounter(models.Model):
     _name = 'energy.counter'
 
-    project_id = fields.Many2one('crm.lead')
-    project_code = fields.Integer('crm.lead', compute='_compute_project_code', readonly=True, store=True)
-    counter_location = fields.Many2one('resource.dock')
+    project_id = fields.Many2one('crm.lead', store=True)
+    project_code = fields.Char('crm.lead', compute='_compute_project_code', readonly=True, store=True)
+    counter_location = fields.Many2one('resource.dock', store=True)
     date_write_start = fields.Datetime(default=lambda self: fields.datetime.now())
     date_write_stop = fields.Datetime(default=lambda self: fields.datetime.now())
-    date_write_monthend = fields.Datetime(default=lambda self: fields.datetime.now())
     counter_state_start = fields.Integer('Enter the value of the counter on connect')
-    counter_state_stop = fields.Integer('Enter the value of the counter on disconnect')
-    counter_state_monthend = fields.Integer('Enter the value of the counter on the end of the month')
+    counter_state_stop = fields.Integer('Enter the value of the counter on disconnect the month')
+    project_consumption = fields.Integer(compute='_project_consumption', readonly=True, store=True)
+
+    counter_type = fields.Selection([('electrical', 'Electrical'),
+                                    ('water', 'Water'),
+                                    ('technical_water', 'Technical Water')],
+                                     string='Meter type',
+                                     required=True,
+                                     default='electrical')
+
+    @api.depends('counter_state_start', 'counter_state_stop')
+    def _project_consumption(self):
+        for counter in self:
+            if counter.counter_state_stop == 0:
+                counter.project_consumption = False
+            else:
+                counter.project_consumption = counter.counter_state_stop - counter.counter_state_start
 
     @api.depends('project_id')
     def _compute_project_code(self):
@@ -28,6 +42,23 @@ class EnergyCounter(models.Model):
             else:
                 counter.project_code = False
 
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    consumption_electrical = fields.One2many('energy.counter',
+                                             'project_id',
+                                             domain=[('counter_type', '=', 'electrical')],
+                                             readonly=True,
+                                             )
+    consumption_water = fields.One2many('energy.counter',
+                                        'project_id',
+                                        domain=[('counter_type', '=', 'water')],
+                                        readonly=True)
+    consumption_technical_water = fields.One2many('energy.counter',
+                                                  'project_id',
+                                                  domain=[('counter_type', '=', 'technical_water')],
+                                                  readonly=True)
 
 
     #counter_type = fields.Selection([('energy', 'Energy counter'), ('water', 'Water counter')],
